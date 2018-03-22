c899406ac19f2426cb6d8d03bbc9da26d92963d4
chevdor <chevdor@gmail.com>
2018-03-22 18:29:50 +0100

    Initial commit - working version

---
+++ index.js
@@ -0,0 +1,41 @@
+const { spawn } = require('child_process');
+
+class Ethup {
+    constructor() {
+        this.proc = null
+    }
+
+    checkAndKill(data, regexp, done) {
+        var re = new RegExp(regexp)
+
+        if (re.test(data.toString())) {
+            this.proc.kill()
+            if (done) done(0)
+        }
+    }
+
+    run(executable, args, regexp, done) {
+        this.proc = spawn(executable, args, { shell: true })
+
+        this.proc.stdout.on('data', (data) => {
+            process.stdout.write(data.toString())
+            this.checkAndKill(data.toString(), regexp, done)
+        })
+
+        this.proc.stderr.on('data', (data) => {
+            process.stdout.write(data.toString())
+            this.checkAndKill(data.toString(), regexp, done)
+        })
+
+        this.proc.on('exit', (status) => {
+            console.log(`Child exited with code ${status}`)
+            if (done) done(status)
+        })
+    }
+
+    kill() {
+    	this.proc.kill()
+    }
+}
+
+module.exports = Ethup;
